648fbba06f8a2f3e01b07bf1344f6f0549097ff1
LS-T <leo.s.teruel@gmail.com>
2021-05-14 16:18:35 -0400

    Added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>Type in the search bar to search through the Directory</p>
+    </div>
+  );
+}
+
+
+export default Header;
