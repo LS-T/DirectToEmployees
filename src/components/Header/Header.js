47efe3e6750439cacdd1737f878b403ef3108bfd
LS-T <leo.s.teruel@gmail.com>
2021-05-14 21:01:09 -0400

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ function Header() {
     <div className="header">
       <h1>Employee Directory</h1>
       <p>Type in the search bar to search through the Directory</p>
+      <p>Click on Name or D.O.B to sort by respective column</p>
     </div>
   );
 }
